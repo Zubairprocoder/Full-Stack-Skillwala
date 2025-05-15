072463da47fc89d25dcd3f6257f5f2e1e70d4103
Zubairprocoder <zubairfreelancer9@gmail.com>
2025-05-15 21:26:26 +0500

    start react js and npm folder

---
+++ Npm/index.js
@@ -0,0 +1,14 @@
+// let z = "Zubair Coder";
+// let prompt = require("prompt-sync")();
+// let password = require("password");
+// // prompt("Hello " + z + "!");
+// console.log(password(2));
+
+let cowsay = require("cowsay");
+const hello = cowsay.say({
+  text: "Hello Indian Female!",
+});
+console.log(hello);
+
+
+// npm node pacakage manager
